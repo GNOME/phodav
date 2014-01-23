1e6d543423c8bf886b1c3e338e11006402a30158
Marc-André Lureau <marcandre.lureau@gmail.com>
2014-01-23 15:26:21 +0100

    spice-webdavd: rename mdns service name

--- spice/spice-webdavd.c
+++ spice/spice-webdavd.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 #include "config.h"
 
 #include <stdlib.h>
@@ -493,14 +509,14 @@ mdns_register_service(void)
     if (!mdns_group) {
         mdns_group = ga_entry_group_new();
 
-        if (!ga_entry_group_attach (mdns_group, mdns_client, &error)) {
+        if (!ga_entry_group_attach(mdns_group, mdns_client, &error)) {
             g_warning("Could not attach MDNS group to client: %s", error->message);
             g_clear_error(&error);
             goto end;
         }
     }
 
-    name = g_strdup_printf("%s\'s public share", g_get_user_name ());
+    name = g_strdup_printf("Spice client folder");
     mdns_service = ga_entry_group_add_service(mdns_group,
                                               name, "_webdav._tcp",
                                               port, &error,
