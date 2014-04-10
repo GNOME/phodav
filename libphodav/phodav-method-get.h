030c02a181b61e1b5b6c43ae7c222aa220fa6a5f
Marc-André Lureau <marcandre.lureau@gmail.com>
2014-04-10 16:54:17 +0200

    wip: move get method

---
+++ libphodav/phodav-method-get.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2; -*- */
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __PHODAV_METHOD_GET_H__
+#define __PHODAV_METHOD_GET_H__
+
+#include <libsoup/soup.h>
+#include "phodav-priv.h"
+
+G_BEGIN_DECLS
+
+gint                  phodav_method_get         (SoupMessage *msg, GFile *file,
+                                                 GCancellable *cancellable, GError **err);
+G_END_DECLS
+
+#endif /* __PHODAV_METHOD_GET_H__ */
